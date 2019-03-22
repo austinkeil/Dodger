ac8ea5002b97a0dd4f924ce3a6370b2298887a34
Austin Keil <austin.c.keil@gmail.com>
2019-03-22 01:32:36 -0700

    added scorekeeping, need to figure out images for good vs. bad

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    private int score;
+
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        scoreText.text = "SCORE: " + score;
+    }
+    public void UpdateScore(int increment)
+    {
+    	 score += increment;
+         scoreText.text = "Score: " + score;
+    }
+}
